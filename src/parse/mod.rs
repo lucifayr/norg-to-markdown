14e69e800f307da38b79304923fe6e9ad2ab3eb5
jackboxx <lucygschwantner@gmail.com>
2023-02-15 22:09:44 +0100

    Separate web and file link parsing

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,6 +1,9 @@
 use self::{
-    parse_headings::parse_headings, parse_highlighting::parse_highlight, parse_links::parse_links,
-    parse_ol::parse_ol, parse_todo::parse_todo,
+    parse_headings::parse_headings,
+    parse_highlighting::parse_highlight,
+    parse_links::{parse_file_links, parse_web_links},
+    parse_ol::parse_ol,
+    parse_todo::parse_todo,
 };
 
 pub mod parse_headings;
@@ -13,6 +16,7 @@ pub fn parse(line: &str) -> Result<String, regex::Error> {
     let headings = parse_headings(line)?;
     let highlights = parse_highlight(&headings)?;
     let ol_list = parse_ol(&highlights)?;
-    let links = parse_links(&ol_list)?;
-    parse_todo(&links)
+    let file_links = parse_file_links(&ol_list)?;
+    let web_links = parse_web_links(&file_links)?;
+    parse_todo(&web_links)
 }
--- src/parse/parse_links.rs
+++ src/parse/parse_links.rs
@@ -1,6 +1,10 @@
 use regex::Regex;
 
-pub fn parse_links(line: &str) -> Result<String, regex::Error> {
+pub fn parse_web_links(line: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(r"\{(.+):\}")?;
+    Ok(regex.replace_all(line, "[$1]($1)").to_string())
+}
+pub fn parse_file_links(line: &str) -> Result<String, regex::Error> {
     let regex = Regex::new(r"\{:(.+):\}")?;
     Ok(regex.replace_all(line, "[$1]($1.md)").to_string())
 }
@@ -10,11 +14,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_links() {
-        assert_eq!(parse_links("text"), Ok("text".to_owned()));
-        assert_eq!(parse_links("- list item"), Ok("- list item".to_owned()));
-        assert_eq!(parse_links("{:link:}"), Ok("[link](link.md)".to_owned()));
-        assert_eq!(parse_links(":{link}:"), Ok(":{link}:".to_owned()));
-        assert_eq!(parse_links("{:link}:"), Ok("{:link}:".to_owned()));
+    fn test_parse_web_links() {
+        assert_eq!(parse_web_links("text"), Ok("text".to_owned()));
+        assert_eq!(parse_web_links("- list item"), Ok("- list item".to_owned()));
+        assert_eq!(parse_web_links("{link:}"), Ok("[link](link)".to_owned()));
+        assert_eq!(parse_web_links(":{link}:"), Ok(":{link}:".to_owned()));
+        assert_eq!(parse_web_links("{:link}:"), Ok("{:link}:".to_owned()));
+    }
+
+    #[test]
+    fn test_parse_file_links() {
+        assert_eq!(parse_file_links("text"), Ok("text".to_owned()));
+        assert_eq!(
+            parse_file_links("- list item"),
+            Ok("- list item".to_owned())
+        );
+        assert_eq!(
+            parse_file_links("{:link:}"),
+            Ok("[link](link.md)".to_owned())
+        );
+        assert_eq!(parse_file_links(":{link}:"), Ok(":{link}:".to_owned()));
+        assert_eq!(parse_file_links("{:link}:"), Ok("{:link}:".to_owned()));
     }
 }
