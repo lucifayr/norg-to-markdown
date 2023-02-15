e020fe849be48e84e70786f857021608e8ee8c10
jackboxx <lucygschwantner@gmail.com>
2023-02-15 23:32:51 +0100

    Update example file

--- src/post_process/post_process_code.rs
+++ src/post_process/post_process_code.rs
@@ -1,12 +1,8 @@
 use regex::Regex;
 
 pub fn post_process_code(text: &str) -> Result<String, regex::Error> {
-    let regex = Regex::new(
-        r"(?ms:^(?P<s_pad>\s*)@code(?P<lang> \w+)?.*?\n(?P<code>.*?)\n^(?P<e_pad>\s*)@end)",
-    )?;
-    Ok(regex
-        .replace_all(text, "$s_pad```$lang\n$code\n$e_pad```")
-        .to_string())
+    let regex = Regex::new(r"(?ms:^(\s*)@code(?P<lang> \w+)?.*?\n(?P<code>.*?)\n^(\s*)@end)")?;
+    Ok(regex.replace_all(text, "```$lang\n$code\n```").to_string())
 }
 
 #[cfg(test)]
@@ -17,92 +13,57 @@ mod tests {
     fn test_post_process_code() {
         assert_eq!(post_process_code("text"), Ok("text".to_owned()));
 
-        let eg = "
-@code
+        let eg = "@code
 const var = 12;
 console.log(var);
-@end
-";
-        let res = "
-```
+@end";
+        let res = "```
 const var = 12;
 console.log(var);
-```
-";
+```";
 
         assert_eq!(post_process_code(eg), Ok(res.to_owned()));
 
-        let eg = "
-        @code
-        const var = 12;
-        console.log(var);
-        @end
-";
-        let res = "
-        ```
-        const var = 12;
-        console.log(var);
-        ```
-";
+        let eg = "@code rust
+let var = 12;
+println!(\"{var}\");
+@end";
+        let res = "``` rust
+let var = 12;
+println!(\"{var}\");
+```";
 
         assert_eq!(post_process_code(eg), Ok(res.to_owned()));
 
-        let eg = "
-@code rust
+        let eg = "@code rust
 let var = 12;
 println!(\"{var}\");
-@end
-";
-        let res = "
-``` rust
+invalid@end";
+        let res = "@code rust
 let var = 12;
 println!(\"{var}\");
-```
-";
+invalid@end";
 
         assert_eq!(post_process_code(eg), Ok(res.to_owned()));
 
-        let eg = "
-@code rust
+        let eg = "invalid@code rust
 let var = 12;
 println!(\"{var}\");
-invalid@end
-";
-        let res = "
-@code rust
+@end";
+        let res = "invalid@code rust
 let var = 12;
 println!(\"{var}\");
-invalid@end
-";
+@end";
 
         assert_eq!(post_process_code(eg), Ok(res.to_owned()));
-
-        let eg = "
-invalid@code rust
+        let eg = "@code rust valid
 let var = 12;
 println!(\"{var}\");
-@end
-";
-        let res = "
-invalid@code rust
+@end";
+        let res = "``` rust
 let var = 12;
 println!(\"{var}\");
-@end
-";
-
-        assert_eq!(post_process_code(eg), Ok(res.to_owned()));
-        let eg = "
-@code rust valid
-let var = 12;
-println!(\"{var}\");
-@end
-";
-        let res = "
-``` rust
-let var = 12;
-println!(\"{var}\");
-```
-";
+```";
 
         assert_eq!(post_process_code(eg), Ok(res.to_owned()));
     }
