1967de9e8a7de4f18537f7a0efc57bafdb90ecd3
jackboxx <lucygschwantner@gmail.com>
2023-02-27 21:21:53 +0100

    Add top and bottom space to heading

--- src/post_process/post_process_headings.rs
+++ src/post_process/post_process_headings.rs
@@ -1,28 +1,11 @@
 use regex::Regex;
 
 pub fn post_process_headings(text: &str) -> Result<String, regex::Error> {
-    let regex = Regex::new(r"(?P<heading>#{1, 6}\s.+[\n\r])(?P<text>.+)")?;
+    let heading_regex = Regex::new(r"(?P<heading>#{1, 6}\s.+[\n\r])")?;
+    let space_regex = Regex::new(r"([ \t]*[\n\r]){3,}")?;
 
-    // This prevents sequential Headings from having inconsistent spacing
-    // due to the 2. Heading being captured as text
-    // e.g.:
-    // # First Heading
-    // ## Second Heading
-    // ### First Heading
-    // #### Second Heading
-    // =====================>
-    // - 1. Match
-    // ------------------
-    // # First Heading
-    // ## Second Heading
-    // - 2. Match
-    // ------------------
-    // ### First Heading
-    // #### Second Heading
-    let first_pass = regex.replace_all(text, "$heading\n$text").to_string();
-    Ok(regex
-        .replace_all(&first_pass, "$heading\n$text")
-        .to_string())
+    let spaced_text = heading_regex.replace_all(text, "\n$heading\n").to_string();
+    Ok(space_regex.replace_all(&spaced_text, "\n\n").to_string())
 }
 
 #[cfg(test)]
@@ -32,17 +15,17 @@ mod tests {
     #[test]
     fn test_post_process_headings() {
         let eg = "## Heading\nsome text without a newline above.";
-        let res = "## Heading\n\nsome text without a newline above.";
+        let res = "\n## Heading\n\nsome text without a newline above.";
 
         assert_eq!(post_process_headings(eg), Ok(res.to_owned()));
 
         let eg = "## Heading\n\nsome text with a newline above.";
-        let res = "## Heading\n\nsome text with a newline above.";
+        let res = "\n## Heading\n\nsome text with a newline above.";
 
         assert_eq!(post_process_headings(eg), Ok(res.to_owned()));
 
-        let eg = "##Not Heading\nsome text that shouldn't get newline above.";
-        let res = "##Not Heading\nsome text that shouldn't get newline above.";
+        let eg = "\n##Not Heading\nsome text that shouldn't get newline above.";
+        let res = "\n##Not Heading\nsome text that shouldn't get newline above.";
 
         assert_eq!(post_process_headings(eg), Ok(res.to_owned()));
     }
